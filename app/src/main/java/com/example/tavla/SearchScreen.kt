6f20801e4aee73d5978a5931ba38917cb188cb15
JH <jonas.hagelid@gmail.com>
2025-02-23 13:20:03 +0100

    some styling, clickable list elements

--- app/src/main/java/com/example/tavla/SearchScreen.kt
+++ app/src/main/java/com/example/tavla/SearchScreen.kt
@@ -1,5 +1,6 @@
 package com.example.tavla
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -10,6 +11,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 
 
 @Composable
@@ -35,14 +38,27 @@ fun SearchScreen(viewModel: ViewModel, modifier: Modifier = Modifier) {
             items(stops) { stop ->
                 val name = stop.properties?.label ?: "Empty value"
                 val categories = stop.properties?.category ?: emptyList()
+                Card(
+                    modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        println("Clicked on stop: $name")
+                    }
+                    .padding(8.dp),
+                    elevation = CardDefaults.cardElevation(4.dp)){
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Text(text = name)
+                        LazyRow {
+                            items(
+                                categories) { category ->
+                                Text(text = category, modifier = Modifier.padding(end = 8.dp))                    }
+                        }
+                    }
 
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(text = name)
-                LazyRow {
-                    items(
-                    categories) { category ->
-                        Text(text = category, modifier = Modifier.padding(end = 8.dp))                    }
                 }
+
             }
         }
     }
